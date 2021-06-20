91cd22fa7cbd44d540975d38df97f3a8a1dfa3a4
권예빈 <kwonay11@naver.com>
2021-06-20 21:22:01 +0900

    회원가입 Firebase 연결

--- app/src/main/java/com/example/hometraningcheck/RegisterActivity.java
+++ app/src/main/java/com/example/hometraningcheck/RegisterActivity.java
@@ -1,16 +1,74 @@
 package com.example.hometraningcheck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private static final String TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.registerButton).setOnClickListener(onClickListener);
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.registerButton:
+//                    Log.e("클릭","클릭");
+                    Register();
+                    break;
+            }
+        }
+    };
+    private void Register(){
+    //일반 뷰는 getText를 하지 못해서 EditText로 형변환
+        String email = ((EditText)findViewById(R.id.emailText)).getText().toString();
+        String password = ((EditText)findViewById(R.id.passwordText)).getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //성공했을 때 UI로직
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                          
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            // 실패했을 때 UI로직
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                        }
+                    }
+                });
+    }
+
 }
