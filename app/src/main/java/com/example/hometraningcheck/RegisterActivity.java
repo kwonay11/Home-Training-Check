e41f2345f6e119687aeeb4e8e8ca8bb7adca92bd
권예빈 <kwonay11@naver.com>
2021-06-20 09:39:41 +0900

    회원가입 디자인

---
+++ app/src/main/java/com/example/hometraningcheck/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.example.hometraningcheck;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
