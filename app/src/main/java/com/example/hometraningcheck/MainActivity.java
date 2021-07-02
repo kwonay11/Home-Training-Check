731c15ed75585903fb830eb4de25831ad08908f7
yebeen <kwonay11@naver.com>
2021-07-02 16:03:27 +0900

    메뉴, 회원정보등록

--- app/src/main/java/com/example/hometraningcheck/MainActivity.java
+++ app/src/main/java/com/example/hometraningcheck/MainActivity.java
@@ -1,21 +1,62 @@
 package com.example.hometraningcheck;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserInfo;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+    //메뉴
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //메뉴,버튼 연결
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerView = (View)findViewById(R.id.drawer);
+
+        Button btn_open = (Button)findViewById(R.id.btn_open_menu);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { //메뉴 버튼이 눌리게 되면
+                drawerLayout.openDrawer(drawerView); // 옆에 슬라이드가 열림
+            }
+        });
+
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        Button btn_close = (Button)findViewById(R.id.btn_close_menu);
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.closeDrawers();
+            }
+        });
+//메뉴 슬라이드 끝
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -25,7 +66,33 @@ public class MainActivity extends AppCompatActivity {
         }else{
             // 유저 존재하면
             //사용자 프로필 정보 가져오기
-            myStartActivity(MemberActivity.class);
+            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            DocumentReference docRef = db.collection("users").document(user.getUid());
+            Task<DocumentSnapshot> documentSnapshotTask = docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if(document != null){
+                            if (document.exists()) {
+                                Log.d(TAG, "DocumentSnapshot data: " + document.getData());//getData에 데이터 들어옴
+                            } else {
+                                Log.d(TAG, "No such document");
+                                myStartActivity(MemberActivity.class); //데이터가 없으면 실행
+                            }
+
+                        }
+                        if (document.exists()) {
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+
 
         }
         findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
@@ -48,7 +115,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void myStartActivity(Class c){
         Intent intent = new Intent(this,c);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //전에 했던 엑티비티 삭제
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //전에 했던 엑티비티 삭제
         startActivity(intent);
     }
+
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
--- app/src/main/java/com/example/hometraningcheck/MemberActivity.java
+++ app/src/main/java/com/example/hometraningcheck/MemberActivity.java
@@ -15,6 +15,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
 public class MemberActivity extends AppCompatActivity {
     private static final String TAG = "MemberInitActivity";
 
@@ -23,15 +24,23 @@ public class MemberActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_init);
 
+        findViewById(R.id.checkButton).setOnClickListener(onClickListener);
         findViewById(R.id.gotoPasswordButton).setOnClickListener(onClickListener);
 
+
+
+    }
+    @Override
+    public void onBackPressed(){ //뒤로가기 버튼누르면 앱 나가기
+        super.onBackPressed();
+        finish();
     }
 
     View.OnClickListener onClickListener = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
             switch (v.getId()){
-                case R.id.checkButton: //로그인 엑티비티로 보내기
+                case R.id.checkButton:
                     profile();
                     break;
                 case R.id.gotoPasswordButton: //비밀번호 변경 엑티비티로 보내기
@@ -50,10 +59,9 @@ public class MemberActivity extends AppCompatActivity {
         String wishDate = ((EditText)findViewById(R.id.wishtDateText)).getText().toString();
 
 
-
         if(name.length() > 0 && recentWeight.length() > 0 && wishWeight.length() > 0 && wishDate.length() > 0){
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
+            FirebaseFirestore db = FirebaseFirestore.getInstance(); //데이터베이스 초기화
 
             MemberInfo memberInfo = new MemberInfo(name,recentWeight,wishWeight,wishDate);
             if (user != null){
@@ -62,14 +70,14 @@ public class MemberActivity extends AppCompatActivity {
                             @Override
                             public void onSuccess(Void aVoid) {
                                 startTost("회원 정보 등록에 성공하였습니다.");
-
+                                finish(); //등록하면 화면 꺼지기
                             }
                         })
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 startTost("회원 정보 등록에 실패하였습니다.");
-                                Log.w(TAG, "Error writing document", e);
+//                                Log.w(TAG, "Error writing document", e);
                             }
                         });
             }
@@ -90,5 +98,7 @@ public class MemberActivity extends AppCompatActivity {
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //전에 했던 엑티비티 삭제
         startActivity(intent);
     }
+
+
 
 }
